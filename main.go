846214418694c7360c6f2c156c422e8dfd870727
abisekhsubedi <abisekh.work@gmail.com>
2023-01-15 21:48:11 +0545

    Configure mongodb and Fiber

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"context"
 
-	"github.com/abisekhsubedi/database"
+	"github.com/abisekhsubedi/rest-api/database"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // loadEnv loads the .env file
@@ -45,8 +46,17 @@ func main() {
 
 	app := fiber.New()
 
+	// routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendFile("./index.html")
 	})
+	app.Post("/api", func(c *fiber.Ctx) error {
+		// Write a todo to the application
+		sampleDoc := bson.M{"name": "Sample todo"}
+		collection := database.GetCollection("todos")
+		nDoc, err := collection.InsertOne(context.TODO(), sampleDoc)
+		return c.SendString("Hello, World 👋!")
+	})
+
 	app.Listen(":3000")
 }
